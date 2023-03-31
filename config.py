60f556510e527d9f3f5228385296855b15ae8e32
oming04 <96978897+oming04@users.noreply.github.com>
2023-03-31 12:56:27 +0800

    Update config.py

--- config.py
+++ config.py
@@ -14,7 +14,7 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 class Config(object):
 
     # Get a bot token from botfather
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "5858667400:AAFNhrQCUDyCeDqwUDop3pqd8cWXobz6teI")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6044156147:AAHyPcegp892CnRbr3xmVM9qNZON8drKgCU")
 
     # Get from my.telegram.org
     APP_ID = int(os.environ.get("APP_ID", "7350916"))
@@ -26,7 +26,7 @@ class Config(object):
     AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5178642977").split())
 
     # Generate a user session string
-    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQGm8CMAxjWyscG0fyHQDl6CG9hWJPIkilRJaHZKKLhEFwS7XVDi03lrgSdg47LxFkLqYKqUBq67_G3btpg5WkGDjpfY6Yg-7gv5Qbl_9L7NwbrxMR7VUFbpqMaLsxE3CI_r5MHgqZfa1nY1FdY9cvW4frihWap2MaPSeTjZQwDyiuhY8Ap81K3NwshhyXqI0n9PKGlECABFtjWv0_MloDRDmiH8_LNixplUX7qybVq9Ppt_A5a933lbpUJd9vCx6DMOtAqx1RJx0YWFi0HRFhR_Di2WqCQodI92J7a1u842GSBobjdBCOjdG7zkn0M_6OtzrNjzFhgxhIlxrAy800HuwdjBdQAAAAFRfeP5AA")
+    TG_USER_SESSION = os.environ.get("TG_USER_SESSION", "BQAkl4IC4LcDLorQ_FDLSkxx4fE4FCUZ-2woH4Gmy0ezbQHSJqUbgiV-_0vmpNo54ffXCifCpOzTcG9o4mJqqTkbzx_5SgtWdXvOMFYJ-8wMSg_eWVlyjgxwQUiLHP_Dt6OuebK_OlX0sFIniWsank59XDsWWIH1MpTsmc9TX7BUKlBhKQR1TfXki8ONqJHtYzK9UDXZfMmZlnHVPa27bjUhDuC_N7T7Hog3TFkj7j7cfQcpx5kMV6YM85hX4XsMFt1gPrVlf6tQtcbMuWCHu9jKHd6rW3ey_PxxarkvEcGeEtsEwxnJgw9j0F3BFrZzyulgMKr1-gZ8rE0hEKzd3fAAAAAT5_f78A")
     
 
 def LOGGER(name: str) -> logging.Logger:
